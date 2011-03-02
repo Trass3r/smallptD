f8d0849d9857390a82c047f4629ff308c59c8443
Trass3r <un@known.com>
2011-03-02 22:41:28 +0100

    on Windows more stack space is needed

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @del .deps
 @del .objs /Q
 
-xfbuild +v smallpt.d +obin\smallptD +xcore +xstd -I..\cl4d -release -O -inline
+xfbuild +v smallpt.d +obin\smallptD +xcore +xstd -I..\cl4d -debug -g -L/STACK:20971520 && cv2pdb -D2 bin\smallptD.exe
 pause
