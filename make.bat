fd5169354ad64a520c9a1db0bb4bbb5508b89b73
Trass3r <un@known.com>
2011-08-08 01:12:19 +0200

    cleaned up makefile

--- make.bat
+++ make.bat
@@ -1,5 +1,2 @@
-@del .deps
-@del .objs /Q
-
-xfbuild +v smallpt.d +obin\smallptD +xcore +xstd -I..\cl4d -debug -g -L/STACK:20971520 && cv2pdb -D2 bin\smallptD.exe
+dmd -release -O -inline -L/STACK:20971520 -ofsmallptD smallpt.d
 pause
